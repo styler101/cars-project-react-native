e2cfa9a7155d0e5be376f0c75a229fd9bf663fd3
Lucas <lucas.santos@mundowap.com.br>
2022-12-23 09:28:27 -0300

    add header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,55 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { fonts, sizes } from "../../styles/global";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+import teslaLogo from "../../assets/img/logo.png";
+import menuLogo from "../../assets/img/menu.png";
+import { fonts, sizes, spacings } from "../../styles/global";
+
+const isAndroid = Platform.OS === "android" ? StatusBar.currentHeight : 0;
+const { width } = Dimensions.get("screen");
 
 export default function App() {
   return (
-    <View>
-      <Text style={styles.title}> Tesla App</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={teslaLogo} style={styles.logo} />
+        <Image source={menuLogo} style={styles.menu} />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: isAndroid,
+  },
+
+  header: {
+    width: width,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: spacings.sm,
+  },
+
+  logo: {
+    width: 110,
+    height: 30,
+    resizeMode: "contain",
+  },
+
+  menu: {
+    width: 30,
+    height: 30,
+    resizeMode: "contain",
+  },
+});
---
+++ src/data/cars.js
@@ -0,0 +1,28 @@
+export default {
+  data: [
+    {
+      id: 1,
+      name: "Model 3",
+      description: "Oder Online for TouchLess Delivery",
+      image: require("../assets/img/Model3.jpeg"),
+    },
+    {
+      id: 2,
+      name: "Model S",
+      description: "Oder Online for TouchLess Delivery",
+      image: require("../assets/img/ModelS.jpeg"),
+    },
+    {
+      id: 3,
+      name: "Model X",
+      description: "Oder Online for TouchLess Delivery",
+      image: require("../assets/img/ModelX.jpeg"),
+    },
+    {
+      id: 4,
+      name: "Model Y",
+      description: "Oder Online for TouchLess Delivery",
+      image: require("../assets/img/ModelY.jpeg"),
+    },
+  ],
+};
