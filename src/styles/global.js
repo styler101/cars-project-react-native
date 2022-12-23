6d8fd40c4bd0eb7d3d434b33990da20862ace096
Lucas <lucas.santos@mundowap.com.br>
2022-12-23 08:55:47 -0300

    add style

---
+++ src/styles/global.js
@@ -0,0 +1,48 @@
+export const sizes = {
+  xsm: 8,
+  xs: 12,
+  sm: 14,
+  base: 16,
+  lg: 18,
+  xl: 20,
+  "2xl": 24,
+  "3xl": 30,
+  "4xl": 36,
+  "5xl": 48,
+  "6xl": 64,
+};
+
+export const spacings = {
+  xsm: 8,
+  xs: 12,
+  sm: 14,
+  base: 16,
+  lg: 18,
+  xl: 20,
+  "2xl": 24,
+  "3xl": 30,
+  "4xl": 36,
+  "5xl": 48,
+  "6xl": 64,
+};
+
+export const fonts = {
+  300: "Roboto_400_Thin",
+  400: "Roboto_Regular",
+  500: "Roboto_Regular500",
+  700: "Roboto_Regular700",
+};
+
+export const colors = {
+  white: "#fff",
+  red: "#cc0000",
+  black: "#1a1a1d",
+  gray: {
+    300: "#cccccc",
+    400: "#999999",
+    600: "#666666",
+    700: "#333333",
+  },
+};
+
+export { colors, fonts, sizes, spacings };
